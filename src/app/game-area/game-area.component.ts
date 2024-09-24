19402260a9254493ef049a322b63721b872172f0
olafmyszak <myszakolaf@gmail.com>
2024-09-25 01:18:34 +0200

    Add total and successful attempts counters

--- src/app/game-area/game-area.component.ts
+++ src/app/game-area/game-area.component.ts
@@ -1,10 +1,9 @@
-import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {WordGeneratorService} from "../word-generator.service";
 import {CdkTrapFocus} from "@angular/cdk/a11y";
-import {interval, Subscription, takeWhile, tap} from "rxjs";
 
 @Component({
   selector: 'app-game-area',
@@ -18,44 +17,68 @@ import {interval, Subscription, takeWhile, tap} from "rxjs";
   templateUrl: './game-area.component.html',
   styleUrl: './game-area.component.css'
 })
-export class GameAreaComponent implements OnInit {
+export class GameAreaComponent {
   gameStarted: boolean = false;
+
   randomWord: string = "";
   userInput: string = "";
-  timeIntervalSeconds: number = 10;
+
+  timeIntervalSeconds: number = 5;
   timeLeft = this.timeIntervalSeconds * 1000;
+  interval: number = 0;
+
+  totalAttempts: number = 0;
+  successfulAttempts: number = 0;
 
   wordGeneratorService = inject(WordGeneratorService);
   cdr = inject(ChangeDetectorRef);
 
-  ngOnInit() {
+  startGame(): void {
+    this.gameStarted = true;
     this.generateRandomWord();
     this.startTimer();
+  }
+
+  stopGame(): void {
+    this.gameStarted = false;
+    clearInterval(this.interval);
   }
 
   generateRandomWord() {
     this.wordGeneratorService.getRandomWord().subscribe((word) => {
       this.randomWord = word;
+      this.userInput = "";
     })
   }
 
   onInput() {
-    if (this.userInput === this.randomWord) {
-      this.generateRandomWord();
-      this.userInput = "";
-      this.timeLeft = this.timeIntervalSeconds * 1000;
-      this.startTimer();
+    let incorrect: boolean = false;
+
+    for(let i=0; i<this.userInput.length; ++i) {
+      if(i >= this.randomWord.length) {
+        incorrect = true;
+        //make this char red
+      }
     }
   }
 
   startTimer() {
-   interval(100).pipe(
+    this.timeLeft = this.timeIntervalSeconds * 1000;
 
-      takeWhile(() => this.timeLeft > 0),
-      tap(() => this.timeLeft -= 100),
+    clearInterval(this.interval);
 
-    ).subscribe()
+    this.interval = setInterval(() => {
+      if (this.timeLeft <= 0) {
+        clearInterval(this.interval);
+        ++this.totalAttempts;
+        this.generateRandomWord();
+        this.cdr.markForCheck();
+        this.startTimer();
+        return;
+      }
+
+      this.timeLeft -= 100;
+      this.cdr.markForCheck();
+    }, 100);
   }
-
-
 }
